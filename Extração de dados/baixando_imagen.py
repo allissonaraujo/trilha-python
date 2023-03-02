3a2fca288d0f076d2fbbed0b6f0b13e886b1204e
Allisson Rafael de Brito Araujo <allissonads@outlook.com>
2023-03-02 16:45:22 -0300

    update: baixando_imagen.py

--- Extração de dados/baixando_imagen.py
+++ Extração de dados/baixando_imagen.py
@@ -1,18 +1,26 @@
+#Imports libs[Selenium]
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import urllib.request
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+#Open link in browser
 driver.get("https://imdb.com/title/tt0120338/mediaindex?ref=tt_pv_mi_sm")
 
+#Implicitly wait of 3 seconds
 driver.implicitly_wait(3)
 
+#Find DIV with ghumbnail images
 divImagens = driver.find_element_by_id("media_index_thumbnail_grid")
+
+#Get First Image 
 primeiraImagem = divImagens.find_elements_by_tag_name("img")[1]
+#Get Attribute SRC
 atributoSrc = primeiraImagem.get_attribute("src")
+#Prin in screen the attribute
 print(atributoSrc)
 
+#Make download with urllib request with verification with try and except
 try:
     urllib.request.urlretrieve(atributoSrc, r"C:\Users\allisson\Downloads\imagem.jpg")
     print("Imagem baixada com sucesso!")
