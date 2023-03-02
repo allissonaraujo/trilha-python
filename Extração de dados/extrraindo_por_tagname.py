10f681a536c3ca8327dc12767f531e964abd1a89
Allisson Rafael de Brito Araujo <allissonads@outlook.com>
2023-03-02 18:04:48 -0300

    update 8 files and delete 1 file

--- Extração de dados/extrraindo_por_tagname.py
+++ Extração de dados/extrraindo_por_tagname.py
@@ -1,18 +1,22 @@
+# Import libs
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from colorama import  Fore, Style
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+# Open link in browser
 driver.get("https://statusinvest.com.br/fundos-imobiliarios/knri11")
 
+# Find elements by tag name and get the text inside of first element
 nomeFii = driver.find_elements_by_tag_name("h1")[0].text
 
+# Find elements by tag name and get the text inside of first element
 valorAtual = driver.find_elements_by_tag_name("strong")[0].text
 
+# Find elements by tag name and get the text inside of first element
 tableaRendimentos = driver.find_elements_by_tag_name("table")[0].text
 
-
+# Print all data extracted in screen and using the colorama lib for set colors in some words
 print(Fore.RED + "Nome da FII:" + Style.RESET_ALL)
 print(nomeFii)
 print("----------------------------")
