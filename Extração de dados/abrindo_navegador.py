e6c4426abe2b93fbd113fe47d2d3a4df2b0a26f9
Allisson Rafael de Brito Araujo <allissonads@outlook.com>
2023-03-02 16:38:20 -0300

    update: abrindo_navegador.py"

--- Extração de dados/abrindo_navegador.py
+++ Extração de dados/abrindo_navegador.py
@@ -1,9 +1,11 @@
+#Imports [Selenium Webdriver]
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+#Open link in browser
 driver.get('https://www.infomoney.com.br')
 
+#Implicitly wait of 3 seconds
 driver.implicitly_wait(3)
 
