f907f42d8c9f91ad8236cc5c391f68c77270b510
Allisson Rafael de Brito Araujo <allissonads@outlook.com>
2023-03-03 17:18:47 -0300

    update baixando_imagen.py"

--- Extração de dados/baixando_imagen.py
+++ Extração de dados/baixando_imagen.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import urllib.request
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
